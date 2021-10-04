697ea28603d5fcec9d39e844bab28aa63e21b180
numanakoluk <numanakoluk01@gmail.com>
2021-10-04 19:28:44 +0300

    Category List

--- Controllers/KategoriController.cs
+++ Controllers/KategoriController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDYNAMICPROJECT.Models.Entity;
 
 namespace MVCDYNAMICPROJECT.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
-            return View();
+            var kategoriler = db.TBLKATEGORILER.ToList();
+
+            return View(kategoriler);
         }
     }
 }
