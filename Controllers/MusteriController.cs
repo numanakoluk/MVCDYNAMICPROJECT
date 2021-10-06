8c12553a49c6b249726ad98072255c8ba6cd97bb
numanakoluk <numanakoluk01@gmail.com>
2021-10-06 17:57:03 +0300

    List And Link

---
+++ Controllers/MusteriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDYNAMICPROJECT.Models.Entity;
+
+namespace MVCDYNAMICPROJECT.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var musteri = db.TBLMUSTERILER.ToList();
+
+            return View(musteri);
+        }
+    }
+}
---
+++ Controllers/UrunlerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDYNAMICPROJECT.Models.Entity;
+
+namespace MVCDYNAMICPROJECT.Controllers
+{
+    public class UrunlerController : Controller
+    {
+
+        // GET: Urunler
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index()
+        {
+            var urunler = db.TBLURUNLER.ToList();
+               
+            return View(urunler);
+        }
+    }
+}
