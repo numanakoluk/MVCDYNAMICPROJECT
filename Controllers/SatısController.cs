180415dd05bf3d17f41a8f36ee0dfa2355505a9b
numanakoluk <numanakoluk01@gmail.com>
2021-10-09 23:55:33 +0300

    PopUpModel2

--- Controllers/SatısController.cs
+++ Controllers/SatısController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDYNAMICPROJECT.Models.Entity;
 
 namespace MVCDYNAMICPROJECT.Controllers
 {
     public class SatısController : Controller
     {
         // GET: Satıs
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
             return View();
+        }
+       [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+
         }
     }
 }
